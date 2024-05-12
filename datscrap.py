5f1b24bfea9b3cac2aa44d1bd56f952057914e53
IsThatYouCarl <90243903+IsThatYouCarl@users.noreply.github.com>
2024-05-12 19:41:11 +0800

    Update datscrap.py

--- datscrap.py
+++ datscrap.py
@@ -50,38 +50,111 @@ def get_table(url_league, df):
 
         print(year)
 
-def get_bundesliga():
+def get_topscorer(Season, League, Nationality, Age_Group, Position, Main_Position, page):
+    saison_id = int(Seasons[Season])
+    OptionKey = int(League_groups[League])
+
+    altersklasse_val = Age_group[Age_Group]
+    if altersklasse_val == "":
+        altersklasse = altersklasse_val
+    else:
+        altersklasse = int(altersklasse_val)
+
+    aurischtung_val = Position_groups[Position]
+    if aurischtung_val == "":
+        aurischtung = aurischtung_val
+    else:
+        aurischtung = int(aurischtung_val)
+
+    spielerposition_id_val = Main_position_list[Main_Position]
+    if spielerposition_id_val == "":
+        spielerposition_id = spielerposition_id_val 
+    else:
+        spielerposition_id = int(spielerposition_id_val)
+
+    land_id = int(Nationality_list[Nationality])
+
+    if str(page) == "All":
+        df_top_scorer =  pd.DataFrame(columns = ['Rank','Name', 'Age', 'Club', 'Matches', 'Goals'])
+        info = f'Season = {Season}, League = {League}, Nationality = {Nationality}, Age Group = {Age_Group}, Position = {Position}, Main Position = {Main_Position}, Number of pages = {page}'
+        current_page = 1
+        print(f"Created a dataframe for {info}")
+        while True:
+            url = f'https://www.transfermarkt.com/scorer/toptorschuetzen/statistik/2023/plus/0/galerie/0?saison_id={saison_id}&selectedOptionKey={OptionKey}&land_id={land_id}&altersklasse={altersklasse}&ausrichtung={aurischtung}&spielerposition_id={spielerposition_id}&yt0=Show/plus/0/galerie/0/page/{current_page}'
+            top_scorer_collection(df_top_scorer, url)
+            print(f'page = {current_page}')
+            current_page +=1
+
+    else:        
+        df_top_scorer =  pd.DataFrame(columns = ['Rank','Name', 'Age', 'Club', 'Matches', 'Goals'])
+        info = f'Season = {Season}, League = {League}, Nationality = {Nationality}, Age Group = {Age_Group}, Position = {Position}, Main Position = {Main_Position}, Number of pages = {page}'
+        print(f"Created a dataframe for {info}")
+        for current_page in range(1, page+1):
+            url = f'https://www.transfermarkt.com/scorer/toptorschuetzen/statistik/2023/ajax/yw1/saison_id/{saison_id}/selectedOptionKey/{OptionKey}/land_id/{land_id}/altersklasse/{altersklasse}/ausrichtung/{aurischtung}/spielerposition_id/{spielerposition_id}/yt0/Show/plus/0/galerie/0/page/{current_page}'
+            print(url)
+            top_scorer_collection(df_top_scorer, url)
+            print(f'page = {current_page}')
+
+    csv_path = 'top_scorer.csv'
+    df_top_scorer.to_csv(csv_path, index = False)
+
+
+def top_scorer_collection(df_top_scorer, url):   
+    headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KTML, Like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
+    page = requests.get(url, headers = headers)
+    soup = BeautifulSoup(page.text, "lxml")
+
+    odd_list = []
+    even_list = []
+    odds = soup.findAll("tr", attrs = {"class": "odd"})
+    evens = soup.findAll("tr", attrs = {"class": "even"})
+
+    for odd in odds:
+        player_list = []
+        rank = odd.findAll("td", attrs={"class":"zentriert"})[0]
+        age = odd.findAll("td", attrs={"class":"zentriert"})[1]
+        matches = odd.findAll("td", attrs={"class":"zentriert"})[3]
+
+        goals = odd.findAll("td", attrs={"class": "zentriert hauptlink"})[0]
+
+        name = odd.findAll("td", attrs = {"class":"hauptlink"})[0]
+        club = odd.findAll("td", attrs = {"class":"hauptlink"})[1]
+        player_list = [rank.text.strip(), name.text.strip(), club.text.strip(), age.text.strip(), matches.text.strip(), goals.text.strip()]
+        odd_list.append(player_list)
+
+    for even in evens:
+        player_list = []
+        rank =even.findAll("td", attrs={"class":"zentriert"})[0]
+        age = even.findAll("td", attrs={"class":"zentriert"})[1]
+        matches = even.findAll("td", attrs={"class":"zentriert"})[3]
+
+        goals = even.findAll("td", attrs={"class": "zentriert hauptlink"})[0]
+
+        name = even.findAll("td", attrs = {"class":"hauptlink"})[0]
+        club = even.findAll("td", attrs = {"class":"hauptlink"})[1]
+        player_list = [rank.text.strip(), name.text.strip(), club.text.strip(), age.text.strip(), matches.text.strip(), goals.text.strip()]
+        even_list.append(player_list)
+
+    master_list = []
+    for i in range(max(len(odd_list), len(even_list))):
+        if len(even_list[i:]) == 0:
+            master_list.append(odd_list[i])
+            break
+        master_list.append(odd_list[i])
+        master_list.append(even_list[i])
+
+    for item in master_list: 
+        new_row = {"Rank": int(item[0]), "Name": item[1], "Age":item[3], "Club":item[2], "Matches":item[4], "Goals":item[5]}
+        df_top_scorer.loc[len(df_top_scorer)] = new_row
+
+def get_league_table(league_name):
     df = pd.DataFrame(columns = ['Season_End_Year','Team', 'Rank', 'MP', 'W', 'D', 'L', 'GD', 'Pts'])
-    get_table('https://www.transfermarkt.com/bundesliga/tabelle/wettbewerb/L1?saison_id=', df)
-    csv_path = 'bundesliga-league-tables.csv'
-    df.to_csv(csv_path, index = False)
-    
-def get_serieA():
-    df = pd.DataFrame(columns = ['Season_End_Year','Team', 'Rank', 'MP', 'W', 'D', 'L', 'GD', 'Pts'])
-    get_table('https://www.transfermarkt.com/serie-a/tabelle/wettbewerb/IT1?saison_id=', df)
-    csv_path = 'serieA-league-tables.csv'
+    league_code = Leagues[league_name]
+    url = f'https://www.transfermarkt.com/bundesliga/tabelle/wettbewerb/{league_code}?saison_id='
+    get_table(url, df)
+    csv_path = f'{league_name}-league-tableslmao.csv'
     df.to_csv(csv_path, index = False)
 
-def get_laliga():
-    df = pd.DataFrame(columns = ['Season_End_Year','Team', 'Rank', 'MP', 'W', 'D', 'L', 'GD', 'Pts'])
-    get_table('https://www.transfermarkt.com/laliga/tabelle/wettbewerb/ES1?saison_id=', df)
-    csv_path = 'laliga-league-tables.csv'
-    df.to_csv(csv_path, index = False)
-    
-def get_ligue1():
-    df = pd.DataFrame(columns = ['Season_End_Year','Team', 'Rank', 'MP', 'W', 'D', 'L', 'GD', 'Pts'])
-    get_table('https://www.transfermarkt.com/ligue-1/tabelle/wettbewerb/FR1?saison_id=', df)
-    csv_path = 'ligue1-league-tables.csv'
-    df.to_csv(csv_path, index = False)
-
-def get_premier_league():
-    df = pd.DataFrame(columns = ['Season_End_Year','Team', 'Rank', 'MP', 'W', 'D', 'L', 'GD', 'Pts'])
-    get_table('https://www.transfermarkt.com/premier-league/tabelle/wettbewerb/GB1?saison_id=', df)
-    csv_path = 'premier-league-tables.csv'
-    df.to_csv(csv_path, index = False)
-
-# get_bundesliga()
-# get_laliga()
-# get_ligue1()
-# get_serieA()
-# get_premier_league()
+get_league_table("bundesliga")
+get_topscorer("23/24","Top 5 leagues", "All nationalities", "All age groups","All positions","All positions", 5)
+#refer to url_data.py for all the options
